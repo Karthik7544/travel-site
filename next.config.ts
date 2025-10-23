2529da00a6bb860826d9d83369cff76a1c6233b8
Karthik7544 <karthik101106@gmail.com>
2025-10-23 11:16:36 +0530

    Initial commit

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: 'export', // enables static HTML export
+  // output: 'export', // enables static HTML export
 };
 
 export default nextConfig;
